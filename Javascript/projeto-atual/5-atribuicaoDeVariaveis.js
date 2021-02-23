282a6668b7c50f3f83c617ca56990a8a737af969
renanmuniz <renan.mmerlo@gmail.com>
2021-02-23 16:43:00 -0300

    Curso 01 aula 02

---
+++ Javascript/projeto-atual/5-atribuicaoDeVariaveis.js
@@ -0,0 +1,11 @@
+console.log("Atribuição de Variáveis");
+
+const idade = 30;
+const primeiroNome = "Renan";
+const sobrenome = "Merlo";
+
+console.log(primeiroNome + " " + sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}.`);
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(`Meu nome é ${nomeCompleto}.`);
